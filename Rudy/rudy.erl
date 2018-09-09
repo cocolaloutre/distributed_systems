2fae19e8a35f70d423e6bee62f6783eb70fd6fc6
cocolaloutre <corentin.nsg.leroy@gmail.com>
2018-09-09 15:00:45 +0200

    Test multiple handlers

--- Rudy/rudy.erl
+++ Rudy/rudy.erl
@@ -1,33 +1,65 @@
 -module(rudy).
--export([init/1, reply/1]).
--export([start/1, stop/0]).
+-export([start/1, start/2, stop/0]).
 
+% Starts the server
 start(Port) ->
-  register(rudy, spawn(fun() -> init(Port) end)).
+  start(Port, 1).
+start(Port, NbHandlers) ->
+  register(rudy, spawn(fun() -> init(Port, NbHandlers) end)).
 
+% Stops the server
 stop() ->
   exit(whereis(rudy), "time to die").
 
-init(Port) ->
+% Creates a socket and the handlers in case of a connection
+init(Port, NbHandlers) ->
   Opt = [list, {active, false}, {reuseaddr, true}],
   case gen_tcp:listen(Port, Opt) of
     {ok, Listen} ->
-      handler(Listen),
+      spawnHandlers(Listen, NbHandlers, self()),
+      checkHandlers(NbHandlers),
+      %handler(Listen),
       gen_tcp:close(Listen),
       ok;
     {error, Error} ->
       io:format("Init error: ~w~n", [Error])
   end.
 
-handler(Listen) ->
+% Creates multiple handler processes
+spawnHandlers(Listen, NbHandlers, Pid) ->
+  case NbHandlers of
+    0 ->
+      ok;
+    _ ->
+      spawn(fun() -> handler(Listen, Pid) end),
+      %spawn(rudy, handler, [Listen, Pid]),
+      %handler(Listen),
+      spawnHandlers(Listen, NbHandlers-1, Pid)
+  end.
+
+% Checks if the handlers are done
+checkHandlers(NbHandlers) ->
+  case NbHandlers of
+    0 ->
+      ok;
+    _ ->
+      receive
+        done ->
+          checkHandlers(NbHandlers-1)
+      end.
+
+% Handles the connection
+handler(Listen, Pid) ->
   case gen_tcp:accept(Listen) of
     {ok, Client} ->
       request(Client),
-      handler(Listen);
+      handler(Listen, Pid);
     {error, Error} ->
       io:format("Handler error: ~w~n", [Error])
-  end.
+  end,
+  Pid ! done.
 
+% Handles the client request
 request(Client) ->
   Recv = gen_tcp:recv(Client, 0),
   case Recv of
@@ -40,6 +72,7 @@ request(Client) ->
   end,
   gen_tcp:close(Client).
 
+% Replies to the client
 reply({{get, URI, _}, _, Body}) ->
   %io:format("Request received for ~s~n", [URI]),
   timer:sleep(40),
