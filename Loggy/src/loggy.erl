df1fc1c247e88ad8cc45465cb38d8b45f64b72f6
cocolaloutre <corentin.nsg.leroy@gmail.com>
2018-09-25 22:56:43 +0200

    Ordered log

--- Loggy/src/loggy.erl
+++ Loggy/src/loggy.erl
@@ -1,4 +1,6 @@
 -module(loggy).
+-import(lists, [append/2, nth/2, keysort/2, delete/2]).
+-import(time, [clock/1, update/3, safe/2]).
 -export([start/1, stop/1]).
 
 start(Nodes) ->
@@ -7,14 +9,26 @@ start(Nodes) ->
 stop(Logger) ->
   Logger ! stop.
 
-init(_) ->
-  loop().
+init(Nodes) ->
+  Clock = time:clock(Nodes),
+  loop([], Clock).
 
-loop() ->
+loop(Msg_queue, Clock) ->
   receive
     {log, From, Time, Msg} ->
-      log(From, Time, Msg),
-      loop();
+      io:format("\t{~w, ~w, ~p}\n", [From, Time, Msg]),
+      New_clock = time:update(From, Time, Clock),
+      io:format("\t\tClock: ~w\n", [New_clock]),
+      Msgs = lists:keysort(2, lists:append(Msg_queue, [{From, Time, Msg}])),
+      Head_msg = nth(1, Msgs),
+      case time:safe(element(2, Head_msg), Msgs, New_clock) of
+        false ->
+          New_msg_queue = Msgs;
+        true ->
+          log(element(1, Head_msg), element(2, Head_msg), element(3, Head_msg)),
+          New_msg_queue = lists:delete(Head_msg, Msgs)
+      end,
+      loop(New_msg_queue, New_clock);
     stop ->
       ok
   end.
--- Loggy/src/time.erl
+++ Loggy/src/time.erl
@@ -1,5 +1,6 @@
 -module(time).
--export([zero/0, inc/2, merge/2, leq/2]).
+-import(lists, [map/2, keyfind/3, keydelete/3, append/2]).
+-export([zero/0, inc/2, merge/2, leq/2, clock/1, update/3, safe/3]).
 
 zero() ->
   0.
@@ -21,4 +22,21 @@ leq(Ti, Tj) ->
       true;
     true ->
       false
+  end.
+
+clock(Nodes) ->
+  lists:map(fun(Name) -> {Name, 0} end, Nodes).
+
+update(Node, Time, Clock) ->
+  lists:keyreplace(Node, 1, Clock, {Node, Time}).
+
+safe(Time, Msg_queue, Clock) ->
+  Min_time = lists:min([T || T <- [element(2, Elem) || Elem <- Clock]]),
+  if
+    Time > Min_time ->
+      false;
+    Time > length(Msg_queue) ->
+      false;
+    true ->
+      true
   end.
