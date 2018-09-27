ef152988c40ceca2d61b4c5001caf8e0eb67d22b
cocolaloutre <corentin.nsg.leroy@gmail.com>
2018-09-27 21:16:25 +0200

    Added gms2

--- Groupy/src/gms2.erl
+++ Groupy/src/gms2.erl
@@ -1,24 +1,33 @@
--module(gms1).
--export([start/1, init/2, start/2, init/3]).
+-module(gms2).
+-export([start/1, start/2]).
+-define(timeout, 100).
+-define(arghh, 100).
 
 start(Id) ->
+  Rnd = random:uniform(1000),
   Self = self(),
-  {ok, spawn_link(fun()-> init(Id, Self) end)}.
+  {ok, spawn_link(fun()-> init(Id, Rnd, Self) end)}.
 
-init(Id, Master) ->
+init(Id, Rnd, Master) ->
+  random:seed(Rnd, Rnd, Rnd),
   leader(Id, Master, [], [Master]).
 
 start(Id, Grp) ->
+  Rnd = random:uniform(1000),
   Self = self(),
-  {ok, spawn_link(fun()-> init(Id, Grp, Self) end)}.
+  {ok, spawn_link(fun()-> init(Id, Rnd, Grp, Self) end)}.
 
-init(Id, Grp, Master) ->
+init(Id, Rnd, Grp, Master) ->
+  random:seed(Rnd, Rnd, Rnd),
   Self = self(),
   Grp ! {join, Master, Self},
   receive
     {view, [Leader|Slaves], Group} ->
+      erlang:monitor(process, Leader),
       Master ! {view, Group},
       slave(Id, Master, Leader, Slaves, Group)
+    after ?timeout ->
+      Master ! {error, "no reply from leader"}
   end.
 
 leader(Id, Master, Slaves, Group) ->
@@ -51,9 +60,32 @@ slave(Id, Master, Leader, Slaves, Group) ->
     {view, [Leader|Slaves2], Group2} ->
       Master ! {view, Group2},
       slave(Id, Master, Leader, Slaves2, Group2);
+    {'DOWN', _Ref, process, Leader, _Reason} ->
+      election(Id, Master, Slaves, Group);
     stop ->
       ok
   end.
 
+election(Id, Master, Slaves, [_|Group]) ->
+  Self = self(),
+  case Slaves of
+    [Self|Rest] ->
+      bcast(Id, {view, Slaves, Group}, Rest),
+      Master ! {view, Group},
+      leader(Id, Master, Rest, Group);
+    [Leader|Rest] ->
+      erlang:monitor(process, Leader),
+      slave(Id, Master, Leader, Rest, Group)
+  end.
+
 bcast(Id, Msg, Nodes) ->
-  lists:foreach(fun(Node) -> Node ! Msg end, Nodes).
+  lists:foreach(fun(Node) -> Node ! Msg, crash(Id) end, Nodes).
+
+crash(Id) ->
+  case random:uniform(?arghh) of
+    ?arghh ->
+      io:format("leader ~w: crash~n", [Id]),
+      exit(no_luck);
+    _ ->
+      ok
+  end.
