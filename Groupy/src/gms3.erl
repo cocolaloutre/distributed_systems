f8643947c1d0dd96a1840c1c69745b160c0c860d
cocolaloutre <corentin.nsg.leroy@gmail.com>
2018-10-03 10:42:51 +0200

    Added gms3

--- Groupy/src/gms3.erl
+++ Groupy/src/gms3.erl
@@ -1,4 +1,6 @@
--module(gms2).
+% view message DOWN message
+
+-module(gms3).
 -export([start/1, start/2]).
 -define(timeout, 100).
 -define(arghh, 1000).
@@ -10,7 +12,7 @@ start(Id) ->
 
 init(Id, Rnd, Master) ->
   random:seed(Rnd, Rnd, Rnd),
-  leader(Id, Master, [], [Master]).
+  leader(Id, Master, 0, [], [Master]).
 
 start(Id, Grp) ->
   Rnd = random:uniform(1000),
@@ -22,64 +24,65 @@ init(Id, Rnd, Grp, Master) ->
   Self = self(),
   Grp ! {join, Master, Self},
   receive
-    {view, [Leader|Slaves], Group} ->
+    {view, N, [Leader|Slaves], Group} ->
       erlang:monitor(process, Leader),
       Master ! {view, Group},
-      slave(Id, Master, Leader, Slaves, Group)
+      slave(Id, Master, Leader, N+1, {view, N, [Leader|Slaves], Group}, Slaves, Group)
     after ?timeout ->
       Master ! {error, "no reply from leader"}
   end.
 
-leader(Id, Master, Slaves, Group) ->
+leader(Id, Master, N, Slaves, Group) ->
   receive
     {mcast, Msg} ->
-      bcast(Id, {msg, Msg}, Slaves),
+      bcast(Id, {msg, N, Msg}, Slaves),
       Master ! Msg,
-      leader(Id, Master, Slaves, Group);
+      leader(Id, Master, N+1, Slaves, Group);
     {join, Wrk, Peer} ->
       Slaves2 = lists:append(Slaves, [Peer]),
       Group2 = lists:append(Group, [Wrk]),
-      bcast(Id, {view, [self()|Slaves2], Group2}, Slaves2),
+      bcast(Id, {view, N, [self()|Slaves2], Group2}, Slaves2),
       Master ! {view, Group2},
-      leader(Id, Master, Slaves2, Group2);
+      leader(Id, Master, N+1, Slaves2, Group2);
     stop ->
       ok
   end.
 
-slave(Id, Master, Leader, Slaves, Group) ->
+slave(Id, Master, Leader, N, Last, Slaves, Group) ->
   receive
     {mcast, Msg} ->
       Leader ! {mcast, Msg},
-      slave(Id, Master, Leader, Slaves, Group);
+      slave(Id, Master, Leader, N, Last, Slaves, Group);
     {join, Wrk, Peer} ->
       Leader ! {join, Wrk, Peer},
-      slave(Id, Master, Leader, Slaves, Group);
-    {msg, Msg} ->
+      slave(Id, Master, Leader, N, Last, Slaves, Group);
+    {msg, I, _} when I < N ->
+      slave(Id, Master, Leader, N, Last, Slaves, Group);
+    {msg, I, Msg} ->
       Master ! Msg,
-      slave(Id, Master, Leader, Slaves, Group);
-    {view, [Leader|Slaves2], Group2} ->
+      slave(Id, Master, Leader, I+1, {msg, I, Msg}, Slaves, Group);
+    {view, I, [Leader|Slaves2], Group2} ->
       Master ! {view, Group2},
-      slave(Id, Master, Leader, Slaves2, Group2);
+      slave(Id, Master, Leader, I+1, {view, I, [Leader|Slaves2], Group2}, Slaves2, Group2);
     {'DOWN', _Ref, process, Leader, _Reason} ->
-      election(Id, Master, Slaves, Group);
+      election(Id, Master, N, Last, Slaves, Group);
     stop ->
       ok
   end.
 
-election(Id, Master, Slaves, [_|Group]) ->
+election(Id, Master, N, Last, Slaves, [_|Group]) ->
   Self = self(),
   case Slaves of
     [Self|Rest] ->
-      bcast(Id, {view, Slaves, Group}, Rest),
+      bcast(Id, Last, Rest),
       Master ! {view, Group},
-      leader(Id, Master, Rest, Group);
+      leader(Id, Master, N, Rest, Group);
     [Leader|Rest] ->
       erlang:monitor(process, Leader),
-      slave(Id, Master, Leader, Rest, Group)
+      slave(Id, Master, Leader, N, Last, Rest, Group)
   end.
 
 bcast(Id, Msg, Nodes) ->
-  %io:format("~w\n", [Msg]),
   lists:foreach(fun(Node) -> Node ! Msg, crash(Id) end, Nodes).
 
 crash(Id) ->
